6a4b4ebcc905756e1f7cda9db33f1aa1320c8753
Devon Minor <devon.minor.00@gmail.com>
2024-08-12 19:43:32 -0400

    Switch firebase to initialize on .env instead

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
+
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
